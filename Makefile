a81d3ef4564580d5e4ba90d884e7aba3c31982dd
Yangyu Chen <cyy@cyyself.name>
2025-11-19 13:40:28 +0800

    use XSTop

--- Makefile
+++ Makefile
@@ -1,26 +1,26 @@
-YQH_FIR = ../yanqihu/build/rtl/SimTop.fir
-NH_FIR = ../nanhu/build/rtl/SimTop.fir
-KMH_FIR = ../kunminghu/build/SimTop.fir
-KMHV2_FIR = ../XiangShan/build/rtl/SimTop.fir
+YQH_FIR = ../yanqihu/build/rtl/XSTop.fir # git checkout 52bb30fe05350d4756db2626949510dd86a9f0bd && make verilog 
+NH_FIR = ../nanhu/build/rtl/XSTop.fir # git checkout ffee184f3d83a9adb2730090d7754bb48b2f33eb && make verilog
+KMH_FIR = ../kunminghu/build/XSTop.fir # git checkout 533652847a406411557b4b8cd1b7c3e7f3712ca6 && make verilog MFC=1
+KMHV2_FIR = ../kunminghu-v2/build/rtl/XSTop.fir # git checkout 316d62e5952bb47fd7224b5fcf4591813a05e08a && make verilog
 
 FIRTOOL = ~/.cache/llvm-firtool/1.62.1/bin/firtool
 FIRTOOL_OPTS = --split-verilog -O=release --disable-annotation-classless --disable-annotation-unknown
 
-result.csv: cal.py verilog/yanqihu/SimTop.sv verilog/nanhu/SimTop.sv verilog/kunminghu/SimTop.sv verilog/kunminghu_v2/SimTop.sv
+result.csv: cal.py verilog/yanqihu/XSTop.sv verilog/nanhu/XSTop.sv verilog/kunminghu/XSTop.sv verilog/kunminghu_v2/XSTop.sv
 	./cal.py > result.csv
 
-verilog/yanqihu/SimTop.sv: $(YQH_FIR)
+verilog/yanqihu/XSTop.sv: $(YQH_FIR)
 	mkdir -p verilog/yanqihu
 	$(FIRTOOL) $(YQH_FIR) -o verilog/yanqihu $(FIRTOOL_OPTS)
 
-verilog/nanhu/SimTop.sv: $(NH_FIR)
+verilog/nanhu/XSTop.sv: $(NH_FIR)
 	mkdir -p verilog/nanhu
 	$(FIRTOOL) $(NH_FIR) -o verilog/nanhu $(FIRTOOL_OPTS)
 
-verilog/kunminghu/SimTop.sv: $(KMH_FIR)
+verilog/kunminghu/XSTop.sv: $(KMH_FIR)
 	mkdir -p verilog/kunminghu
 	$(FIRTOOL) $(KMH_FIR) -o verilog/kunminghu $(FIRTOOL_OPTS)
 
-verilog/kunminghu_v2/SimTop.sv: $(KMHV2_FIR)
+verilog/kunminghu_v2/XSTop.sv: $(KMHV2_FIR)
 	mkdir -p verilog/kunminghu_v2
 	$(FIRTOOL) $(KMHV2_FIR) -o verilog/kunminghu_v2 $(FIRTOOL_OPTS)
--- cal.py
+++ cal.py
@@ -39,23 +39,36 @@ def read_child_module(module_text, module_names):
                 res[instance_name] = module_name
     return res
 
+def read_module_scala_files(module_text):
+    res = set()
+    for line in module_text.splitlines():
+        line_strip = line.strip()
+        comment_pos = line_strip.rfind("// ")
+        if comment_pos != -1 and ".scala:" in line_strip:
+            source_pos = line_strip = line_strip[comment_pos + 3:].strip()
+            line_pos = source_pos.find(":")
+            res.add(source_pos[:line_pos].strip())
+    return res
+
 def get_hier(src_dict):
-    assert "SimTop" in src_dict
+    assert "XSTop" in src_dict
     res = dict() # module_name -> dict(instance_name -> module_name)
-    to_process = ["SimTop"]
+    module_scala = dict()
+    to_process = ["XSTop"]
     while to_process:
         module_name = to_process.pop()
         if module_name in res:
             continue
         module_text = src_dict[module_name]
         child_modules = read_child_module(module_text, src_dict.keys())
+        module_scala[module_name] = read_module_scala_files(module_text)
         res[module_name] = child_modules
         for child_module in child_modules.values():
             if child_module not in res:
                 to_process.append(child_module)
-    return res
+    return (res, module_scala)
 
-def output_in_tree(modules_dict, root_module="SimTop", indent="  ", max_depth=10):
+def output_in_tree(modules_dict, root_module="XSTop", indent="  ", max_depth=10):
     if max_depth == 0:
         return
     print(f"{indent}{root_module}")
@@ -63,52 +76,42 @@ def output_in_tree(modules_dict, root_module="SimTop", indent="  ", max_depth=10
     for instance_name, module_name in child_modules.items():
         output_in_tree(modules_dict, module_name, indent + "  ", max_depth - 1)
 
-def count_flat(cur_module, hier, src_map, res_dict):
+def count_flat(cur_module, hier, scala_files, src_map, res_dict, res_src_dict):
     if cur_module in res_dict:
         return
     res_dict[cur_module] = src_map[cur_module].count("\n") + 1
+    res_src_dict[cur_module] = scala_files[cur_module].copy()
     for child_module in hier.get(cur_module, {}).values():
-        count_flat(child_module, hier, src_map, res_dict)
+        count_flat(child_module, hier, scala_files, src_map, res_dict, res_src_dict)
         res_dict[cur_module] += res_dict[child_module]
+        res_src_dict[cur_module].update(res_src_dict[child_module])
 
-if __name__ == "__main__":
-    yqh, nh, kmh, kmhv2 = dict(), dict(), dict(), dict()
-    for path in pathlib.Path("verilog/yanqihu").rglob("*.sv"):
-        file = path.name
-        with open(path, "r") as f:
-            yqh_part = split_src(f.read())
-            yqh.update(yqh_part)
-    for path in pathlib.Path("verilog/nanhu").rglob("*.sv"):
-        file = path.name
-        with open(path, "r") as f:
-            nh_part = split_src(f.read())
-            nh.update(nh_part)
-    for path in pathlib.Path("verilog/kunminghu").rglob("*.sv"):
-        file = path.name
-        with open(path, "r") as f:
-            kmh_part = split_src(f.read())
-            kmh.update(kmh_part)
-    for path in pathlib.Path("verilog/kunminghu_v2").rglob("*.sv"):
-        file = path.name
-        with open(path, "r") as f:
-            kmhv2_part = split_src(f.read())
-            kmhv2.update(kmhv2_part)
-    yqh_hier = get_hier(yqh)
-    nh_hier = get_hier(nh)
-    kmh_hier = get_hier(kmh)
-    kmhv2_hier = get_hier(kmhv2)
-    # output_in_tree(yqh_hier)
-    # output_in_tree(nh_hier)
-    # output_in_tree(kmh_hier)
-    # output_in_tree(kmhv2_hier)
-    yqh_count = dict()
-    nh_count = dict()
-    kmh_count = dict()
-    kmhv2_count = dict()
-    count_flat("SimTop", yqh_hier, yqh, yqh_count)
-    count_flat("SimTop", nh_hier, nh, nh_count)
-    count_flat("SimTop", kmh_hier, kmh, kmh_count)
-    count_flat("SimTop", kmhv2_hier, kmhv2, kmhv2_count)
+def cal_src_count(src_dict, src_count_dict, default_folder=None):
+    line_cache = dict()
+    for module_name, scala_files in src_dict.items():
+        for scala_file in scala_files:
+            if scala_file in line_cache:
+                src_count_dict[module_name] = src_count_dict.get(module_name, 0) + line_cache[scala_file]
+            else:
+                read_success = False
+                for prefix in ['/', default_folder]:
+                    if prefix is None:
+                        continue
+                    try:
+                        with open(f"{prefix}{scala_file}", "r") as f:
+                            line_count = sum(1 for _ in f)
+                            line_cache[scala_file] = line_count
+                            src_count_dict[module_name] = src_count_dict.get(module_name, 0) + line_count
+                            read_success = True
+                            break
+                    except FileNotFoundError:
+                        continue
+                if not read_success:
+                    print(f"Warning: Scala source file {scala_file} not found.")
+                    line_cache[scala_file] = 0
+                    src_count_dict[module_name] = 0
+
+def cal_count(yqh_count, nh_count, kmh_count, kmhv2_count):
     res = {
         "yqh": {
             "Frontend": yqh_count["Frontend"] - yqh_count["ICache"],
@@ -153,3 +156,48 @@ if __name__ == "__main__":
         dcache = res[part]["DCache"]
         l2top = res[part]["L2Top"]
         print(f"{part},{frontend},{backend},{memblock},{icache},{dcache},{l2top}")
+
+if __name__ == "__main__":
+    yqh, nh, kmh, kmhv2 = dict(), dict(), dict(), dict()
+    for path in pathlib.Path("verilog/yanqihu").rglob("*.sv"):
+        file = path.name
+        with open(path, "r") as f:
+            yqh_part = split_src(f.read())
+            yqh.update(yqh_part)
+    for path in pathlib.Path("verilog/nanhu").rglob("*.sv"):
+        file = path.name
+        with open(path, "r") as f:
+            nh_part = split_src(f.read())
+            nh.update(nh_part)
+    for path in pathlib.Path("verilog/kunminghu").rglob("*.sv"):
+        file = path.name
+        with open(path, "r") as f:
+            kmh_part = split_src(f.read())
+            kmh.update(kmh_part)
+    for path in pathlib.Path("verilog/kunminghu_v2").rglob("*.sv"):
+        file = path.name
+        with open(path, "r") as f:
+            kmhv2_part = split_src(f.read())
+            kmhv2.update(kmhv2_part)
+    yqh_hier, yqh_scala = get_hier(yqh)
+    nh_hier, nh_scala = get_hier(nh)
+    kmh_hier, kmh_scala = get_hier(kmh)
+    kmhv2_hier, kmhv2_scala = get_hier(kmhv2)
+    # output_in_tree(yqh_hier)
+    # output_in_tree(nh_hier)
+    # output_in_tree(kmh_hier)
+    # output_in_tree(kmhv2_hier)
+    yqh_count, nh_count, kmh_count, kmhv2_count = dict(), dict(), dict(), dict()
+    yqh_src, nh_src, kmh_src, kmhv2_src = dict(), dict(), dict(), dict()
+    count_flat("XSTop", yqh_hier, yqh_scala, yqh, yqh_count, yqh_src)
+    count_flat("XSTop", nh_hier, nh_scala, nh, nh_count, nh_src)
+    count_flat("XSTop", kmh_hier, kmh_scala, kmh, kmh_count, kmh_src)
+    count_flat("XSTop", kmhv2_hier, kmhv2_scala, kmhv2, kmhv2_count, kmhv2_src)
+    yqh_scala_count, nh_scala_count, kmh_scala_count, kmhv2_scala_count = dict(), dict(), dict(), dict()
+    cal_src_count(yqh_src, yqh_scala_count, "/mnt/data/xs/xs-env/yanqihu/")
+    cal_src_count(nh_src, nh_scala_count, "/mnt/data/xs/xs-env/nanhu/")
+    cal_src_count(kmh_src, kmh_scala_count, "/mnt/data/xs/xs-env/kunminghu/")
+    cal_src_count(kmhv2_src, kmhv2_scala_count, "/mnt/data/xs/xs-env/kunminghu-v2/")
+    cal_count(yqh_count, nh_count, kmh_count, kmhv2_count)
+    print("\nScala Source Line Count:")
+    cal_count(yqh_scala_count, nh_scala_count, kmh_scala_count, kmhv2_scala_count)
